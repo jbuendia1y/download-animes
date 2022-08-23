d7b65ba06785195a3172055596b79d931cd63529
jbuendia1y <jgamer669@gmail.com>
2022-08-23 12:39:35 -0500

    + Library data

--- setup.py
+++ setup.py
@@ -12,9 +12,10 @@ setup(
         ]
     },
     packages=find_packages(),
-    author="Joaquín Buendía",
+    author="jbuendia1y (Joaquín Buendía)",
     description="Download your anime",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/jbuendia1y/download-animes",
     keywords=["download", "anime"]
 )
